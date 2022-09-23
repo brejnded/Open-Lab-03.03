306bf02030efacd5117249fd5861ae94ceb6442d
Matúš Nekoranec <113455013+brejnded@users.noreply.github.com>
2022-09-23 17:50:33 +0200

    Update Comparator.cs

--- Open-Lab-03.03/Comparator.cs
+++ Open-Lab-03.03/Comparator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_03._03
 {
@@ -6,6 +6,7 @@ namespace Open_Lab_03._03
     {
         public bool CompareCharactersCount(string str1, string str2)
         {
+           return str1.Length == str2.Length;
             throw new NotImplementedException();
         }
     }
